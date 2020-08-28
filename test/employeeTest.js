ac7e50961ea35023764abc7cf27ee74c1d6dc2e7
olivia-wang-code <2350625418@qq.com>
2020-08-28 21:32:14 +0800

    [homework][olivia]test:add should return correct message when toString given emplyee engineer

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,11 +1,17 @@
 const employeeTest = require('ava');
-const {Employee } = require('../src/employee.js');
+const { Employee } = require('../src/employee.js');
 
-employeeTest('should return correct owes when validateType given a type', t => {
-    try{
-        let employee = new Employee("","oli");
+employeeTest('should return correct message when toString given emplyee engineer', t => {
+    let employee = new Employee("olivia", "engineer");
+    let result = employee.toString();
+    t.is("olivia (engineer)", result);
+})
+
+employeeTest('should return error when toString given a type', t => {
+    try {
+        let employee = new Employee("", "oli");
         t.fail();
-    }catch(e){
-        t.is("Employee cannot be of type oli",e.message);
+    } catch (e) {
+        t.is("Employee cannot be of type oli", e.message);
     }
 })
