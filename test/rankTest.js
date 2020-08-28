fdeb7131d8c13d9d09609da36160f454cf14760c
olivia-wang-code <2350625418@qq.com>
2020-08-28 19:43:36 +0800

    [homework][olivia]test:add test should retrn B when rating given voyage with german 19 history length 11

--- test/rankTest.js
+++ test/rankTest.js
@@ -152,6 +152,64 @@ rankTest('test should return B when rating given voyage with china 15 and histor
   t.is('B', result);
 })
 
+rankTest('test should retrn B when rating given voyage with german 19 history length 11', t => {
+  //given
+  const voyage = {
+    zone: 'german',
+    length: 19,
+  };
+  const history = [
+    {
+      zone: 'china',
+      profit: 1,
+    },
+    {
+      zone: 'china',
+      profit: 2,
+    },
+    {
+      zone: 'china',
+      profit: 3,
+    },
+    {
+      zone: 'china',
+      profit: 4,
+    },
+    {
+      zone: 'china',
+      profit: 5,
+    },
+    {
+      zone: 'china',
+      profit: 6,
+    },
+    {
+      zone: 'china',
+      profit: 7,
+    },
+    {
+      zone: 'china',
+      profit: 8,
+    },
+    {
+      zone: 'china',
+      profit: 9,
+    },
+    {
+      zone: 'china',
+      profit: 10,
+    },
+    {
+      zone: 'china',
+      profit: 11,
+    },
+  ];
+    //when
+    let result = rating(voyage, history);
+    //then
+    t.is('B', result);
+  })
+
 
 
 
