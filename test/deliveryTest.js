b842c3b19960046c829d983f8d8af42f65f03b9a
olivia-wang-code <2350625418@qq.com>
2020-08-28 21:17:27 +0800

    [homework][olivia]test:add test should return 3 when deliveryDate given true and MH

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -17,3 +17,18 @@ deliveryTest('test should return 2 when deliveryDate given true and MA', t => {
     t.is(2, result);
 })
 
+deliveryTest('test should return 3 when deliveryDate given true and MH', t => {
+    let isRush = true;
+    let anOrder = {
+        deliveryState: 'NH',
+        placedOn: {
+            plusDays: function (data) {
+                return data;
+            }
+        }
+    }
+
+    let result = deliveryDate(anOrder, isRush)
+
+    t.is(3, result);
+})
